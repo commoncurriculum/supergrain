d7984cf5b6458c68d9064d6fafaee5362758d56c
Scott Ames-Messinger <scott@commoncurriculum.com>
2025-09-03 23:27:39 -0400

    feat: implement DocumentStore class with basic storage to pass tests

--- src/core/store/DocumentStore.ts
+++ src/core/store/DocumentStore.ts
@@ -1,18 +1,27 @@
-import type { Document, DocumentType, DocumentId } from '../types'
+import type { Document, DocumentType, DocumentId, DocumentKey } from '../types'
 
 export class DocumentStore {
+  private documents = new Map<DocumentKey, Document>()
+
+  private getKey(type: DocumentType, id: DocumentId): DocumentKey {
+    return `${type}:${id}`
+  }
+
   setDocument<T extends Document>(
-    _type: DocumentType,
-    _id: DocumentId,
-    _document: T
+    type: DocumentType,
+    id: DocumentId,
+    document: T
   ): void {
-    throw new Error('Not implemented: DocumentStore.setDocument')
+    const key = this.getKey(type, id)
+    this.documents.set(key, document)
   }
 
   getDocument<T extends Document>(
-    _type: DocumentType,
-    _id: DocumentId
+    type: DocumentType,
+    id: DocumentId
   ): T | null {
-    throw new Error('Not implemented: DocumentStore.getDocument')
+    const key = this.getKey(type, id)
+    const document = this.documents.get(key)
+    return document ? (document as T) : null
   }
 }
